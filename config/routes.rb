a01d072c7cd18e30caafa2eb9518f09e5f95944a
Ronald <rjskinnerc@unal.edu.co>
2022-08-15 13:49:25 -0500

    Refactor routes.rb in favor of resources

--- config/routes.rb
+++ config/routes.rb
@@ -4,8 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "users#index"
-  get "/users/:user_id/posts", to: "posts#index", as: "user_posts"
-  get "/users/:user_id/posts/:id", to: "posts#show", as: "user_post"
-  get "/users", to: "users#index"
-  get "/users/:id", to: "users#show", as: "user"
+  resources :users, only: [:index, :show]  do
+    resources :posts, only: [:index, :show] 
+  end
 end
