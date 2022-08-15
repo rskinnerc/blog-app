58821113802eb4314b26b0c5c251bb7f34235196
Ronald <rjskinnerc@unal.edu.co>
2022-08-15 11:22:19 -0500

    Add user /user/:id/posts route an view

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/users/:id/posts", to: "posts#index", as: "user_posts"
   get "/users", to: "users#index"
-
-  get "/users/:id", to: "users#show"
+  get "/users/:id", to: "users#show", as: "user"
 end
