27bdb867b1605beb3078101078426e2e2fc189ac
Ronald <rjskinnerc@unal.edu.co>
2022-08-15 14:05:01 -0500

    Add tests for /sers/:user_id/posts/:id route

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts nested routes', type: :request do
-  context 'GET /users/1/posts' do
+  context 'GET /users/:user_id/posts' do
     it 'returns a 200 response' do
       get '/users/1/posts'
       expect(response).to have_http_status(200)
@@ -15,6 +15,23 @@ RSpec.describe 'Posts nested routes', type: :request do
     it 'renders a html containing a <h1>Here is a list of posts for a given user</h1> tag' do
       get '/users/1/posts'
       expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
+    end
+  end
+
+  context 'GET /users/:user_id/posts/:id' do
+    it 'returns a 200 response' do
+      get '/users/1/posts/1'
+      expect(response).to have_http_status(200)
+    end
+
+    it 'renders the show view' do
+      get '/users/1/posts/1'
+      expect(response).to render_template(:show)
+    end
+
+    it 'renders a html containing a <h1>Here is a given post of a given user.</h1> tag' do
+      get '/users/1/posts/1'
+      expect(response.body).to include('<h1>Here is a given post of a given user.</h1>')
     end
   end
 end
