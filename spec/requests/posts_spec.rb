12bb5d1c60cbed961546c693e3f2013ab0d7cafd
Ronald <rjskinnerc@unal.edu.co>
2022-08-22 12:12:46 -0500

    Fix linter errors

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -5,7 +5,8 @@ RSpec.describe 'Posts nested routes', type: :request do
     @user = User.create(id: 1, name: 'Tom',
                         photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
                         bio: 'Lorem ipsum dolor sit amet', postscount: 0)
-    @post = Post.create(id: 1, author: @user, title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit', text: 'Quis hendrerit dolor magna eget est lorem ipsum.', commentscounter: 0, likescounter: 0)
+    @post = Post.create(id: 1, author: @user, title: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit',
+                        text: 'Quis hendrerit dolor magna eget est lorem ipsum.', commentscounter: 0, likescounter: 0)
   end
 
   context 'GET /users/:user_id/posts' do
