be4025a0502b30359cd39d1b586c24ee8a26522d
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-08-23 20:25:12 +0430

    Add test to check the user's bio if exist

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'User listig path', type: :feature do
     before do
         @user = User.create(id: 1, name: 'Tom',
-                              photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', bio: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ullamcorper a lacus vestibulum sed. Etiam sit amet nisl purus in. Arcu vitae elementum curabitur vitae nunc sed velit dignissim sodales..', postscount: 0)
+                              photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1', bio: 'Lorem ipsum dolor sit amet', postscount: 0)
         Post.create(author: @user, title: 'Post 1', text: 'Lorem ipsum dolor sit amet', commentscounter: 0,
                     likescounter: 0)
         Post.create(author: @user, title: 'Post 2', text: 'Lorem ipsum dolor sit amet', commentscounter: 0,
@@ -27,5 +27,9 @@ RSpec.describe 'User listig path', type: :feature do
       it 'should show the user\'s number of posts.' do
         visit user_path(@user)
         expect(page).to have_content('Number of Posts: 5')
+      end
+      it 'should show the user\'s bio.' do
+        visit user_path(@user)
+        expect(page).to have_content('Lorem ipsum dolor sit amet')
       end
 end
