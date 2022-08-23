32c4e48a595fa979b8d30e33aa48b8fc08892e75
Ronald <rjskinnerc@unal.edu.co>
2022-08-23 11:29:40 -0500

    Add test to check the post's title

--- spec/features/posts_spec.rb
+++ spec/features/posts_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'User listig path', type: :feature do
     @user = User.create(id: 1, name: 'Tom',
                         photo: 'https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1',
                         bio: 'Lorem ipsum dolor sit amet', postscount: 0)
-    @post = Post.create(author: @user, title: 'Post 1', text: 'Lorem ipsum dolor sit amet', commentscounter: 0,
+    @post = Post.create(author: @user, title: 'Post 1', text: 'This is the post title', commentscounter: 0,
                         likescounter: 0)
     Comment.create(post: @post, author: @user,
                    text: 'Ullamcorper a lacus vestibulum sed. Etiam sit amet nisl purus in.!')
@@ -14,17 +14,22 @@ RSpec.describe 'User listig path', type: :feature do
   end
 
   it 'should show the user\'s profile picture.' do
-    visit user_path(@user)
+    visit user_posts_path(@user)
     expect(page.find('img')['src']).to have_content('https://images.pexels.com/photos/220453/pexels-photo-220453.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1')
   end
 
   it 'should show the user\'s username.' do
-    visit user_path(@user)
+    visit user_posts_path(@user)
     expect(page).to have_content('Tom')
   end
 
   it 'should show the user\'s number of posts.' do
-    visit user_path(@user)
+    visit user_posts_path(@user)
     expect(page).to have_content('Number of Posts: 1')
+  end
+
+  it 'checks that I can see a post\'s title.' do
+    visit user_posts_path(@user)
+    expect(page).to have_content('This is the post title')
   end
 end
