a021ae6ce56f1ad72923e6a91ace409d568eced4
Ronald <rjskinnerc@unal.edu.co>
2022-08-25 16:50:49 -0500

    Update counters callbacks
    - Refactor view permissions

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,9 +2,14 @@ class Comment < ApplicationRecord
   belongs_to :post
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
-  after_create :update_comments_counter
+  after_create -> { update_author_posts_count(:increment) }
+  after_destroy -> { update_author_posts_count(:decrement) }
 
-  def update_comments_counter
-    post.commentscounter.nil? ? post.update(commentscounter: 1) : post.update(commentscounter: post.commentscounter + 1)
+  def update_author_posts_count(operation)
+    if operation == :increment
+      post.increment!(:commentscounter)
+    else
+      post.decrement!(:commentscounter)
+    end
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,25 @@
 class Post < ApplicationRecord
-  has_many :comments, -> { includes :author }
-  has_many :likes
+  has_many :comments, -> { includes :author }, dependent: :destroy
+  has_many :likes, dependent: :destroy
   belongs_to :author, class_name: 'User', foreign_key: 'author_id'
 
   validates :title, presence: true
   validates :title, length: { maximum: 250 }
   validates :commentscounter, :likescounter, numericality: { greater_than_or_equal_to: 0 }
 
-  after_create :update_author_posts_count
+  after_create -> { update_author_posts_count(:increment) }
+  after_destroy -> { update_author_posts_count(:decrement) }
+
+  def update_author_posts_count(operation)
+    if operation == :increment
+      author.increment!(:postscount)
+    else
+      author.decrement!(:postscount)
+    end
+  end
 
   def most_recent_comments
     comments.limit(5).order(created_at: :desc)
-  end
-
-  def update_author_posts_count
-    author.postscount.nil? ? author.update(postscount: 1) : author.update(postscount: author.postscount + 1)
   end
 
   def liked?(user)
