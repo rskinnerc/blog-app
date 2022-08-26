de405ff2d13e6747347f8be6693706978044a038
Abdul Hadi Jafari <abdulhadijafari2015@gmail.com>
2022-08-26 22:20:30 +0430

    Create an API endpoint to add a comment to a post.

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -3,4 +3,11 @@ class Api::CommentsController < ApplicationController
         @comments = Comment.where(post_id: params[:post_id])
         render json: @comments
     end
+
+    def create
+        p current_user
+        permitted = params.require(:comment).permit(:text)
+        @comment = Comment.new(text: permitted[:text], author_id: 1, post_id: params[:post_id])
+        @comment.save
+    end
 end
