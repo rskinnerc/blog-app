e0b561fa953c356028adfbe922276c145f5ad506
Ronald <rjskinnerc@unal.edu.co>
2022-08-26 13:37:43 -0500

    Install devise-jwt gem
    - Fixed linter errors

--- app/controllers/api/comments_controller.rb
+++ app/controllers/api/comments_controller.rb
@@ -1,13 +1,13 @@
 class Api::CommentsController < ApplicationController
-    def index
-        @comments = Comment.where(post_id: params[:post_id])
-        render json: @comments
-    end
+  def index
+    @comments = Comment.where(post_id: params[:post_id])
+    render json: @comments
+  end
 
-    def create
-        p current_user
-        permitted = params.require(:comment).permit(:text)
-        @comment = Comment.new(text: permitted[:text], author_id: 1, post_id: params[:post_id])
-        @comment.save
-    end
+  def create
+    p current_user
+    permitted = params.require(:comment).permit(:text)
+    @comment = Comment.new(text: permitted[:text], author_id: 1, post_id: params[:post_id])
+    @comment.save
+  end
 end
