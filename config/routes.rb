159f5973a27e72bd5441c335f820b5a7b84ab671
Ronald <rjskinnerc@unal.edu.co>
2022-08-27 11:57:40 -0500

    Install rswag and generate docs

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
   devise_for :users, controllers: { sessions: 'sessions' }, path: '/', path_names: { sign_in: 'login'}
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
