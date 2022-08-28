5accb8581541d86eede3443191f695deb10b8dfd
Ronald <rjskinnerc@unal.edu.co>
2022-08-27 20:51:21 -0500

    Hotfi devisejwt dispatch request config

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -311,5 +311,9 @@ Devise.setup do |config|
 
   config.jwt do |jwt|
     jwt.secret = Rails.application.credentials.devise_jwt_secret_key!
+
+    jwt.dispatch_requests = [
+      ['POST', %r{^/login$}],
+    ]
   end
 end
